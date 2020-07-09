6cb7f9fb598c0474ed0aa0f0c2a2424f63c50489
bighoon <ralxognsa2@gmail.com>
2020-07-10 00:31:49 +0900

    practice_15 commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.scrollview_13)
+        setContentView(R.layout.practice_15)
     }
 }
